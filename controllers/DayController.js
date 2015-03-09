0b2f9e4493eb73e0e2769cde6ab10217663d6f94
James Maynard <student@epicodus.com>
2015-03-09 14:14:29 -0700

    Add deleting functionality to Day Controller.

--- controllers/DayController.js
+++ controllers/DayController.js
@@ -1,3 +1,10 @@
 CommunityCalendar.DayController = Ember.ObjectController.extend({
-
+    actions: {
+      delete: function() {
+        if (confirm('Woah. You wanna do that?')) {
+          this.get('model').destroyRecord();
+        };
+        this.transitionToRoute('calendar')
+      }
+    }
 });
