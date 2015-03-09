4599dbf9b314d73672fef41b0f98c3f80035e6e9
James Maynard <jameskirkmaynard@gmail.com>
2015-03-09 16:05:39 -0700

    Add interest handlebar with delete functionality.

--- controllers/InterestController.js
+++ controllers/InterestController.js
@@ -1,3 +1,10 @@
 CommunityCalendar.InterestController = Ember.ObjectController.extend({
-
+  actions: {
+    delete: function() {
+      if (confirm("Sure about this?")) {
+        this.get('model').destroyRecord();
+      }
+      this.transitionToRoute('day')
+    }
+  }
 });
