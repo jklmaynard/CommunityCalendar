94abdf49878a82746651df8431a6e6d5cafa6720
James Maynard <jameskirkmaynard@gmail.com>
2015-03-10 09:41:29 -0700

    Delete home page and reconfigure routes.

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 CommunityCalendar.Router.map(function() {
-  this.resource('home', {path: '/'});
   this.resource('new-day');
-  this.resource('calendar');
+  this.resource('calendar', {path: '/'});
   this.resource('day', {path: '/:day_id'}, function() {
     this.resource('new-interest')
     this.resource('interest', {path: '/:interest_id'})
